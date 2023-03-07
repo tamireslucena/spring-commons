2c97366292d5e691d59742f65709c225b82287c9
tamireslucena <tamiresb.lucena@gmail.com>
2023-03-07 13:20:25 -0300

    feat: add utils for log

---
+++ src/main/java/com/tamireslucena/springcommons/utils/MaskData.java
@@ -0,0 +1,16 @@
+package com.tamireslucena.springcommons.utils;
+
+import com.tamireslucena.springcommons.enums.MaskPosition;
+import com.tamireslucena.springcommons.enums.TypeOfData;
+
+
+public class MaskData {
+
+    public static String maskData(TypeOfData type, Integer maskSize, MaskPosition position, String data){
+        return "return";
+    }
+
+    public static String maskDocument(TypeOfData type, Integer maskSize, String data){
+        return "return";
+    }
+}
