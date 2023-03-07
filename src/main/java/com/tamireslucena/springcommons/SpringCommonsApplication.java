4da4da831eaecdfb7d68f73ae992dc651d358e04
tamireslucena <tamiresb.lucena@gmail.com>
2023-03-07 09:24:22 -0300

    config: first commit

---
+++ src/main/java/com/tamireslucena/springcommons/SpringCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.tamireslucena.springcommons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommonsApplication.class, args);
+	}
+
+}
